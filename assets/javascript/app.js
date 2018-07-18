71f05d32339cb5105977f0818b49605423e61f10
stephanie <stephanie99tech@gmail.com>
2018-07-17 23:16:10 -0700

    used update js file and updated both html and css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,176 +1,118 @@
-//render a calendar
-var month_olympic = [31,29,31,30,31,30,31,31,30,31,30,31];
-var month_normal = [31,28,31,30,31,30,31,31,30,31,30,31];
-var month_name = ["January","Febrary","March","April","May","June","July","Auguest","September","October","November","December"];
-
-var holder = document.getElementById("days");
-var prev = document.getElementById("prev");
-var next = document.getElementById("next");
-var ctitle = document.getElementById("calendar-title");
-var cyear = document.getElementById("calendar-year");
-
-var my_date = new Date();
-var my_year = my_date.getFullYear();
-var my_month = my_date.getMonth();
-var my_day = my_date.getDate();
-
-function dayStart(month, year) {
-	var tmpDate = new Date(year, month, 1);
-	return (tmpDate.getDay());
-}
-
-function daysMonth(month, year) {
-	
-	if (year%4===0&&year%100!==0||year%400===0) {
-		return (month_olympic[month]);
-	} else {
-		return (month_normal[month]);
-	}
-}
-
-function refreshDate(){
-	var str = "";
-	var totalDay = daysMonth(my_month, my_year); 
-	var firstDay = dayStart(my_month, my_year); 
-	var myclass;
-	for(var i=1; i<firstDay; i++){ 
-		str += "<li></li>"; 
-	}
-	for(var i=1; i<=totalDay; i++){
-		if((i<my_day && my_year==my_date.getFullYear() && my_month==my_date.getMonth()) || my_year<my_date.getFullYear() || ( my_year==my_date.getFullYear() && my_month<my_date.getMonth())){ 
-			myclass = " class='lightgrey'"; 
-		}else if (i==my_day && my_year==my_date.getFullYear() && my_month==my_date.getMonth()){
-			myclass = " class='green greenbox' id='today'"; 
-		}else{
-			myclass = " class='darkgrey'"; 
-		}
-		str += "<li"+myclass+">"+i+"</li>"; 
-	}
-	holder.innerHTML = str; 
-	ctitle.innerHTML = month_name[my_month]; 
-	cyear.innerHTML = my_year; 
-}
-refreshDate(); 
-
-prev.onclick = function(ev){
-	ev.preventDefault();
-	my_month--;
-	if(my_month<0){
-		my_year--;
-		my_month = 11;
-	}
-	refreshDate();
-}
-next.onclick = function(ev){
-	ev.preventDefault();
-	my_month++;
-	if(my_month>11){
-		my_year++;
-		my_month = 0;
-	}
-	refreshDate();
-}
-
 //connect firebase
-  var config = {
-    apiKey: "AIzaSyA2qSkPULRcH8Cx7ovy6jkymtSKEhi3JV8",
-    authDomain: "thoughtkeeper-37286.firebaseapp.com",
-    databaseURL: "https://thoughtkeeper-37286.firebaseio.com",
-    projectId: "thoughtkeeper-37286",
-    storageBucket: "thoughtkeeper-37286.appspot.com",
-    messagingSenderId: "1008945539170"
-  };
-  firebase.initializeApp(config);
-  
-  var dataRef = firebase.database();
-  var userInput = "";
-  var userMood;
-  var date = "";
+var config = {
+  apiKey: "AIzaSyDP9z_IucdD0Aneqog4OQt-tt3BQhS0oEs",
+  authDomain: "thoughtkeeper-80696.firebaseapp.com",
+  databaseURL: "https://thoughtkeeper-80696.firebaseio.com",
+  projectId: "thoughtkeeper-80696",
+  storageBucket: "thoughtkeeper-80696.appspot.com",
+  messagingSenderId: "271875655252"
+};
+firebase.initializeApp(config);
 
-  
-//   var enableClick = function(){
-  
-// }
+var dataRef = firebase.database();
+var userInput = "";
+var date = new Date();
 
 
-$("i").on("click", function(event) {
-    event.preventDefault();
+function getCurrentDateTimeMySql() {        
+  var tzoffset = (new Date()).getTimezoneOffset() * 60000;
+  var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 19).replace('T', ' ');
+  var mySqlDT = localISOTime;
+  return mySqlDT;
+}
+
+
+var dateId = "";
+var dateDisplay = "";
+
+$("#add").on("click", function(event) {
+  event.preventDefault();
+  userInput = $("#thought-input").val();
+  dateId = getCurrentDateTimeMySql().slice(0, 10);
+  dateDisplay = getCurrentDateTimeMySql();
+  $("#thought-input").val("");
+
+  function writeThought() {
+      var newRef = dataRef.ref(dateId + '/' + 'myThought').push();
+      var newKey = newRef.key;
+      var newThought={
+          id: newKey,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP,
+          thought: userInput,         
+          dateId: dateId,
+          dateDisplay: dateDisplay
+      }
+      var updates = {};
+           updates[dateId + '/' + 'myThought' + '/' + dateDisplay]= newThought;
+            return dataRef.ref().update(updates);
+            dataRef.ref('/myThought').update(newThought);
+  }
+
+   if (userInput!== "" ) {
+       writeThought()} 
+       else {
+           alert('nothing is added')
+       };
+
+
+  });
    
-    userMood = $(this).attr("data-mood");
-    console.log(userMood);
+dateId = getCurrentDateTimeMySql().slice(0, 10);
 
-        // $(this).off('click');
-        // setTimeout(enableClick, 3000);
-      
-    
-    date = new Date();
-
-    var dateStr = date.toLocaleString();
-
-    dataRef.ref('/myMood').push({
-      dateAdded: firebase.database.ServerValue.TIMESTAMP,
-      mood: userMood,
-      myDate: dateStr
-    });
-  });
+var  theRef = dataRef.ref(dateId + '/' + 'myThought').orderByKey();
 
 
-  $("#add").on("click", function(event) {
-    event.preventDefault();
+theRef.on("child_added", function(childSnapshot) {
+ 
+//     var content = childSnapshot.val().thought;
+// console.log(content);
 
-    userInput = $("#thought-input").val();
+$('#thought-display').prepend("<div class='wrap'>" + "<div class='my-date'>" + childSnapshot.val().dateDisplay +"</div>" + "<div class='my-thought'> " + childSnapshot.val().thought + "</div>" + "</div>");
 
-    date = new Date();
 
-    var dateStr = date.toLocaleString();
+}, function (error) {
+ console.log("Error: " + error.code);
+});
 
-    
-    $("#thought-input").val("");
 
-    
 
-    dataRef.ref('/myThought').push({
-      dateAdded: firebase.database.ServerValue.TIMESTAMP,
-      thought: userInput,
-      myDate: dateStr
-    });
-  });
+//   dataRef.ref('/myThought').on("child_added", function(childSnapshot) {
+
+//     console.log(childSnapshot.val().thought);
+
+//     $("#thought-display").prepend("<div class='wrap'>" + "<div class='my-date'>" + childSnapshot.val().dateDisplay +"</div>" + "<div class='my-thought'> " + childSnapshot.val().thought + "</div>" + "</div>");
+
+//   }, function(errorObject) {
+//     console.log("Errors handled: " + errorObject.code);
+//   };
+
+//   dataRef.ref('/myMood').on("child_added", function(childSnapshot) {
+
+//     console.log(childSnapshot.val().mood);
+
+//     $("#mood-display").prepend("<div class='mood-wrap'>" + "<div class='display-mood'>" + childSnapshot.val().mood +"</div>" + "</div>");
   
 
-  dataRef.ref('/myThought').on("child_added", function(childSnapshot) {
+//   }, function(errorObject) {
+//     console.log("Errors handled: " + errorObject.code);
+//   });
 
-    console.log(childSnapshot.val().thought);
 
-    $("#full-display").prepend("<div class='wrap'>" + "<div class='my-date'>" + childSnapshot.val().myDate +"</div>" + "<div class='my-thought'> " + childSnapshot.val().thought + "</div>" + "</div>");
+//   $('#food').on('click', function(){
 
-  }, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-  });
+//   })
 
-  dataRef.ref('/myMood').on("child_added", function(childSnapshot) {
+//   $('.checkbox').on('click', function(){
 
-    console.log(childSnapshot.val().mood);
 
-    $("#mood-display").prepend("<div class='mood-wrap'>" + "<div class='display-mood'>" + childSnapshot.val().mood +"</div>" + "</div>");
+//     var userFood = $(this).attr('data-food');
+//     $('#today').append(userFood);
+
+// })
     
-
-  }, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-  });
-
-
-
-      
 //   dataRef.ref().orderByChild("dateAdded").on("child_added", function(childSnapshot) {
 //     // $(".display").text(snapshot.val().userInput);
 //     $("#full-display").prepend("<div class='wrap'>" + "<div class='my-date'>" + childSnapshot.val().myDate +"</div>" + "<div class='my-thought'> " + childSnapshot.val().thought + "</div>" + "</div>");
 // }, function(errorObject) {
 //     console.log("Errors handled: " + errorObject.code);
 //   });
-
-
-
-
-
-
-
